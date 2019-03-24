4f2486b2ad5df40b484637ef0ccee86c97133b18
Randy Little <randylittle@randys-mbp.lan>
2019-03-24 02:08:24 -0500

    attempt two

---
+++ precache-manifest.a455557ceb6758534d802765683dc893.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "27eb3e21e186e955aeb0",
+    "url": "/static/js/main.90cfb755.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ffe420e6d364f0b248bec2bae22301e2",
+    "url": "/index.html"
+  }
+];
